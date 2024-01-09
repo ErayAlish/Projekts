7ec1e4d095f651463fc0a700e3a294d357f475b5
Bin Aktiv Du Passiv ☯︎︎馬 <erayalish8848@gmail.com>
2024-01-09 20:26:08 +0100

    f2

--- Tracking.py
+++ Tracking.py
@@ -4,7 +4,7 @@ from phonenumbers import geocoder
 def get_location(phone_number):
     try:
         parsed_number = phonenumbers.parse(phone_number, None)
-        location = geocoder.description_for_number(parsed_number, "tr")  # Hier "de" für deutsche Ausgabe - Ändere dies entsprechend für andere Sprachen
+        location = geocoder.description_for_number(parsed_number, "de")  # Hier "de" für deutsche Ausgabe - Ändere dies entsprechend für andere Sprachen
         country = geocoder.country_name_for_number(parsed_number, "en")  # "en" für englische Ländernamen - Ändere dies entsprechend für andere Sprachen
         return f"Standort: {location}, Land: {country}"
     except phonenumbers.phonenumberutil.NumberParseException as e:
